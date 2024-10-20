95c70fc699551c6b58cc227ce6faa4add1e74bc7
Reinaldo J. Menendez <rejamen@gmail.com>
2024-10-20 11:51:30 +0200

    Add addons with linter issues

---
+++ addons/external-addons/external_addon_with_issues/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'External Addon with Linter Issues',
+    'summary': 'This is an example of external addon with linter issues',
+    'version': '1.0',
+    'author': 'Reinaldo J. Menendez',
+    'category': 'Extra Tools',
+    'depends': ['base'],
+    'data': [],
+    'auto_install': False,
+    'installable': True,
+}
